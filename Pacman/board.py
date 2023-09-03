20fe7df2155ecc4d98f06c787fb86fdb44f0d524
Eshan <76171637+DarkDevil-1@users.noreply.github.com>
2023-09-03 11:01:27 +0400

    Adding Donkey Kong

---
+++ Pacman/board.py
@@ -0,0 +1,4 @@
+# 0 = empty black rectangle, 1 = dot, 2 = big dot, 3 = vertical line,
+# 4 = horizontal line, 5 = top right, 6 = top left, 7 = bot left, 8 = bot right
+# 9 = gate
+
--- menu.py
+++ menu.py
@@ -12,7 +12,8 @@ def PacMan():
 
 
 def DonkeyKong():
-    pass
+    from DK import main
+    main.Donkey()
 
 
 def Snake():
@@ -22,22 +23,22 @@ def Snake():
 
 root = tk.Tk()
 root.title("Retro Library")
-root.geometry("1200x900")
+root.geometry("1000x1000")
 root.configure(bg="black")
 
 label = tk.Label(root, text="Retro Library", bg="black", fg="green", font=("Courier", 44))
 label.pack(padx=20, pady=20)
 
 button_1 = tk.Button(root, text="Tetris", bg="green", fg="red", font=("Courier", 44), command=Tetris)
-button_1.pack(padx=20, pady=50)
+button_1.pack(padx=20, pady=30)
 
 button_2 = tk.Button(root, text="Pac Man", bg="green", fg="red", font=("Courier", 44), command=PacMan)
-button_2.pack(padx=20, pady=50)
+button_2.pack(padx=20, pady=30)
 
-button_3 = tk.Button(root, text="Donkey Kong", bg="green", fg="red", font=("Courier", 44), command=DonkeyKong)
-button_3.pack(padx=20, pady=50)
+button_3 = tk.Button(root, text="DK", bg="green", fg="red", font=("Courier", 44), command=DonkeyKong)
+button_3.pack(padx=20, pady=30)
 
 button_4 = tk.Button(root, text="Snake", bg="green", fg="red", font=("Courier", 44), command=Snake)
-button_4.pack(padx=20, pady=50)
+button_4.pack(padx=20, pady=30)
 
 root.mainloop()
